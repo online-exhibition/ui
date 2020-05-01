7d010767ec5d463855ea02f566330620e584536b
Exhibition <online-exhibition@dein-hosting.de>
2020-05-01 12:35:20 +0200

    Initial setup of the exhibition UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Exhibition app entry point
+ * @return {object} root DOM representation of the app
+ */
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
